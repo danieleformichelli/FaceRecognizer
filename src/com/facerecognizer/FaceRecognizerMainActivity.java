bf6ebcae9a3984820dc68e6303a019c4b69ab63b
Daniele Formichelli <daniele.formichelli@gmail.com>
2014-05-20 14:21:53 +0200

    Eclipse project created.

---
+++ src/com/facerecognizer/FaceRecognizerMainActivity.java
@@ -0,0 +1,27 @@
+package com.facerecognizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class FaceRecognizerMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_face_recognizer_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.face_recognizer_main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
+
+}
