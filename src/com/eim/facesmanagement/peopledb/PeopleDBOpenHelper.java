a7ed05ecf3e442ab67d58b646440918ed640bbe1
Daniele Formichelli <daniele.formichelli@gmail.com>
2014-06-13 21:43:10 +0200

    PeopleDatabase: fixed onUpgrade.

--- src/com/eim/facesmanagement/peopledb/PeopleDBOpenHelper.java
+++ src/com/eim/facesmanagement/peopledb/PeopleDBOpenHelper.java
@@ -1,40 +1,34 @@
 package com.eim.facesmanagement.peopledb;
 
+import com.eim.facerecognition.LBPHFaceRecognizer;
+
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class PeopleDBOpenHelper extends SQLiteOpenHelper {
-	
-	private static final int VERSION = 2;
+	private static final int VERSION = 1;
 
-	private static final String DB_CREATE_PEOPLE = "CREATE TABLE " 
-			 + FacesContract.People.TABLE + "(" 
-			 + FacesContract.People._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " 
-			 + FacesContract.People.NAME + " TEXT UNIQUE NOT NULL" 
-			 +");";
-	
-	private static final String DB_CREATE_FACES = "CREATE TABLE " 
-			 + FacesContract.Faces.TABLE + " (" 
-			 + FacesContract.Faces._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " 
-			 + FacesContract.Faces.PERSON_ID + " TEXT NOT NULL, "
-			 + FacesContract.Faces.PHOTO_URL + " TEXT UNIQUE NOT NULL, "
-			 + "FOREIGN KEY(" + FacesContract.Faces.PERSON_ID + ") REFERENCES " 
-			 + FacesContract.People.TABLE + "(" + FacesContract.People._ID + ")"
-			 + " ON DELETE CASCADE);";
+	Context mContext;
 
-	// TODO create indexes
-	
+	private static final String DB_CREATE_PEOPLE = "CREATE TABLE "
+			+ FacesContract.People.TABLE + "(" + FacesContract.People._ID
+			+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ FacesContract.People.NAME + " TEXT UNIQUE NOT NULL" + ");";
+
+	private static final String DB_CREATE_FACES = "CREATE TABLE "
+			+ FacesContract.Faces.TABLE + " (" + FacesContract.Faces._ID
+			+ " INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ FacesContract.Faces.PERSON_ID + " TEXT NOT NULL, "
+			+ FacesContract.Faces.PHOTO_URL + " TEXT UNIQUE NOT NULL, "
+			+ "FOREIGN KEY(" + FacesContract.Faces.PERSON_ID + ") REFERENCES "
+			+ FacesContract.People.TABLE + "(" + FacesContract.People._ID + ")"
+			+ " ON DELETE CASCADE);";
+
 	public PeopleDBOpenHelper(Context context) {
 		super(context, FacesContract.DB_NAME, null, VERSION);
+		mContext = context;
 	}
-	
-	public PeopleDBOpenHelper(Context context, String name,
-			CursorFactory factory, int version) {
-		super(context, name, factory, version);
-	}
-
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
@@ -42,12 +36,12 @@ public class PeopleDBOpenHelper extends SQLiteOpenHelper {
 		db.execSQL(DB_CREATE_FACES);
 	}
 
-
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		LBPHFaceRecognizer.getInstance(mContext).train(null);
 		db.execSQL("DROP TABLE IF EXISTS " + FacesContract.Faces.TABLE);
 		db.execSQL("DROP TABLE IF EXISTS " + FacesContract.People.TABLE);
 		onCreate(db);
 	}
-	
+
 }
