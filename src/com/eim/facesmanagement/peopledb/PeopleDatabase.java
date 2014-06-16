17a1894c116681b1770a80a748ec88bfca99ae72
niphis <giovannoni41@gmail.com>
2014-06-16 13:16:37 +0200

    added photo deletion from db functions

--- src/com/eim/facesmanagement/peopledb/PeopleDatabase.java
+++ src/com/eim/facesmanagement/peopledb/PeopleDatabase.java
@@ -1,14 +1,16 @@
 package com.eim.facesmanagement.peopledb;
 
-import android.annotation.TargetApi;
+import java.io.File;
+
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Build;
 import android.util.LongSparseArray;
 
-@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+/**
+ * This class manages the Database containing people and their photos
+ */
 public class PeopleDatabase {
 	private static PeopleDatabase instance;
 	private static SQLiteDatabase db;
@@ -30,6 +32,14 @@ public class PeopleDatabase {
 	private PeopleDatabase() {
 	}
 
+	/**
+	 * Edits the name of a person
+	 * 
+	 * @param id
+	 *            - identifier of the person
+	 * @param newName
+	 *            - new name
+	 */
 	public void editPersonName(long id, String newName) {
 		ContentValues values = new ContentValues();
 		values.put(FacesContract.People.NAME, newName);
@@ -39,13 +49,39 @@ public class PeopleDatabase {
 		db.update(FacesContract.People.TABLE, values, whereClause, whereArgs);
 	}
 
+	/**
+	 * Removes a person and the related photos
+	 * 
+	 * @param id
+	 *            identifier of the person
+	 */
 	public void removePerson(long id) {
-		String whereClause = FacesContract.People._ID + " = ?";
+		String query = "SELECT " + FacesContract.Faces.PHOTO_URL + " FROM "
+				+ FacesContract.Faces.TABLE + " WHERE "
+				+ FacesContract.Faces.PERSON_ID + " = ?";
 		String[] whereArgs = { String.valueOf(id) };
+
+		Cursor c = db.rawQuery(query, whereArgs);
+		int photoUrlIndex = c.getColumnIndex(FacesContract.Faces.PHOTO_URL);
+		while (c.moveToNext()) {
+			String photoUrl = c.getString(photoUrlIndex);
+			File photoFile = new File(photoUrl);
+			if (photoFile != null)
+				photoFile.delete();
+		}
+
+		String whereClause = FacesContract.People._ID + " = ?";
 
 		db.delete(FacesContract.People.TABLE, whereClause, whereArgs);
 	}
 
+	/**
+	 * Adds a person
+	 * 
+	 * @param name
+	 *            name of the person
+	 * @return the identifier of the inserted person
+	 */
 	public long addPerson(String name) {
 		ContentValues values = new ContentValues();
 		values.put(FacesContract.People.NAME, name);
@@ -53,6 +89,15 @@ public class PeopleDatabase {
 		return db.insert(FacesContract.People.TABLE, null, values);
 	}
 
+	/**
+	 * Adds a photo to a person
+	 * 
+	 * @param personId
+	 *            identifier of the person to whom the photo is added
+	 * @param photoUrl
+	 *            url of the photo to be added
+	 * @return the identifier of the photo added
+	 */
 	public long addPhoto(long personId, String photoUrl) {
 		ContentValues values = new ContentValues();
 		values.put(FacesContract.Faces.PERSON_ID, personId);
@@ -61,13 +106,37 @@ public class PeopleDatabase {
 		return db.insert(FacesContract.Faces.TABLE, null, values);
 	}
 
+	/**
+	 * Removes a photo from both the database and the disk
+	 * 
+	 * @param id
+	 *            identifier of the photo to remove
+	 */
 	public void removePhoto(long id) {
-		String whereClause = FacesContract.Faces._ID + " = ?";
+
+		String query = "SELECT " + FacesContract.Faces.PHOTO_URL + " FROM "
+				+ FacesContract.Faces.TABLE + " WHERE "
+				+ FacesContract.Faces._ID + " = ?";
 		String[] whereArgs = { String.valueOf(id) };
+
+		Cursor c = db.rawQuery(query, whereArgs);
+		int photoUrlIndex = c.getColumnIndex(FacesContract.Faces.PHOTO_URL);
+		String photoUrl = c.getString(photoUrlIndex);
+		File photoFile = new File(photoUrl);
+		if (photoFile != null)
+			photoFile.delete();
+
+		String whereClause = FacesContract.Faces._ID + " = ?";
 
 		db.delete(FacesContract.Faces.TABLE, whereClause, whereArgs);
 	}
 
+	/**
+	 * Returns a Person object from its id
+	 * 
+	 * @param id
+	 * @return the Person object, null if the person does not exist
+	 */
 	public Person getPerson(long id) {
 		Person selectedPerson;
 
@@ -104,6 +173,12 @@ public class PeopleDatabase {
 		return selectedPerson;
 	}
 
+	/**
+	 * Returns all the people in the database and their photos
+	 * 
+	 * @return a sparse array in which each key is the identifier of the person
+	 *         stored in the correspondent value
+	 */
 	public LongSparseArray<Person> getPeople() {
 		long currentId = -1;
 		Person currentPerson = null;
@@ -152,7 +227,22 @@ public class PeopleDatabase {
 		return people;
 	}
 
+	/**
+	 * Deletes all the entries from the database and the photos on the disk
+	 */
 	public void clear() {
+		String query = "SELECT " + FacesContract.Faces.PHOTO_URL + " FROM "
+				+ FacesContract.Faces.TABLE;
+
+		Cursor c = db.rawQuery(query, null);
+		int photoUrlIndex = c.getColumnIndex(FacesContract.Faces.PHOTO_URL);
+		while (c.moveToNext()) {
+			String photoUrl = c.getString(photoUrlIndex);
+			File photoFile = new File(photoUrl);
+			if (photoFile != null)
+				photoFile.delete();
+		}
+
 		db.delete(FacesContract.People.TABLE, null, null);
 		db.delete(FacesContract.Faces.TABLE, null, null);
 	}
