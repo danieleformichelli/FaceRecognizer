c45312c5649fb1b9519397f1295f815f2e00c20a
niphis <giovannoni41@gmail.com>
2014-06-16 17:51:26 +0200

    added switch preference

--- src/com/eim/utilities/Preferences.java
+++ src/com/eim/utilities/Preferences.java
@@ -34,23 +34,26 @@ public class Preferences {
 				.valueOf(mContext
 						.getString(R.string.detection_show_confirmation_dialog_default));
 		
-		showDetectionScaleFactorKey = mContext.getString(R.string.detection_scale_factor);
-		showDetectionScaleFactorDefault = mContext.getString(R.string.detection_scale_factor_default);
+		detectionScaleFactorKey = mContext.getString(R.string.detection_scale_factor);
+		detectionScaleFactorDefault = mContext.getString(R.string.detection_scale_factor_default);
 		
-		showDetectionMinNeighborsKey = mContext.getString(R.string.detection_min_neighbors);
-		showDetectionMinNeighborsDefault = mContext.getString(R.string.detection_min_neighbors_default);
+		detectionMinNeighborsKey = mContext.getString(R.string.detection_min_neighbors);
+		detectionMinNeighborsDefault = mContext.getString(R.string.detection_min_neighbors_default);
 		
-		showDetectionMinRelativeFaceSizeKey = mContext.getString(R.string.detection_min_relative_face_size);
-		showDetectionMinRelativeFaceSizeDefault = mContext.getString(R.string.detection_min_relative_face_size_default);
+		detectionMinRelativeFaceSizeKey = mContext.getString(R.string.detection_min_relative_face_size);
+		detectionMinRelativeFaceSizeDefault = mContext.getString(R.string.detection_min_relative_face_size_default);
 		
-		showDetectionMaxRelativeFaceSizeKey = mContext.getString(R.string.detection_max_relative_face_size);
-		showDetectionMaxRelativeFaceSizeDefault = mContext.getString(R.string.detection_max_relative_face_size_default);
+		detectionMaxRelativeFaceSizeKey = mContext.getString(R.string.detection_max_relative_face_size);
+		detectionMaxRelativeFaceSizeDefault = mContext.getString(R.string.detection_max_relative_face_size_default);
 		
-		showManagementNumberOfColumnsLandscapeKey = mContext.getString(R.string.management_number_of_columns_landscape);
-		showManagementNumberOfColumnsLandscapeDefault = mContext.getString(R.string.management_number_of_columns_landscape);
+		detectionDetectorTypeKey = mContext.getString(R.string.detection_detector_type);
+		detectionDetectorTypeDefault = mContext.getString(R.string.detection_detector_type_on);
 		
-		showManagementNumberOfColumnsPortraitKey = mContext.getString(R.string.management_number_of_columns_portrait);
-		showManagementNumberOfColumnsPortraitDefault = mContext.getString(R.string.management_number_of_columns_portrait);
+		managementNumberOfColumnsLandscapeKey = mContext.getString(R.string.management_number_of_gallery_columns_landscape);
+		managementNumberOfColumnsLandscapeDefault = mContext.getString(R.string.management_number_of_gallery_columns_landscape);
+		
+		managementNumberOfColumnsPortraitKey = mContext.getString(R.string.management_number_of_gallery_columns_portrait);
+		managementNumberOfColumnsPortraitDefault = mContext.getString(R.string.management_number_of_gallery_columns_portrait);
 	}
 
 	private String showFacesManagementOnStartupKey;
@@ -69,40 +72,45 @@ public class Preferences {
 				showDetectionConfirmationDialogKey,
 				showDetectionConfirmationDialogDefault);
 	}
-	
-	private String showDetectionScaleFactorKey;
-	private String showDetectionScaleFactorDefault;
-	public String showDetectionScaleFactor () {
-		return mSharedPreferences.getString(showDetectionScaleFactorKey, showDetectionScaleFactorDefault);
+	private String detectionScaleFactorKey;
+	private String detectionScaleFactorDefault;
+	public Double detectionScaleFactor () {
+		return Double.parseDouble(mSharedPreferences.getString(detectionScaleFactorKey, detectionScaleFactorDefault));
 	}
 	
-	private String showDetectionMinNeighborsKey;
-	private String showDetectionMinNeighborsDefault;
-	public String showDetectionMinNeighbors () {
-		return mSharedPreferences.getString(showDetectionMinNeighborsKey , showDetectionMinNeighborsDefault);
+	private String detectionMinNeighborsKey;
+	private String detectionMinNeighborsDefault;
+	public int detectionMinNeighbors () {
+		return Integer.parseInt(mSharedPreferences.getString(detectionMinNeighborsKey , detectionMinNeighborsDefault));
 	}
 	
-	private String showDetectionMinRelativeFaceSizeKey;
-	private String showDetectionMinRelativeFaceSizeDefault;
-	public String showDetectionMinRelativeFaceSize () {
-		return mSharedPreferences.getString(showDetectionMinRelativeFaceSizeKey, showDetectionMinRelativeFaceSizeDefault);
+	private String detectionMinRelativeFaceSizeKey;
+	private String detectionMinRelativeFaceSizeDefault;
+	public double detectionMinRelativeFaceSize () {
+		return Double.parseDouble(mSharedPreferences.getString(detectionMinRelativeFaceSizeKey, detectionMinRelativeFaceSizeDefault));
 	}
 	
-	private String showDetectionMaxRelativeFaceSizeKey;
-	private String showDetectionMaxRelativeFaceSizeDefault;
-	private String showDetectionMaxRelativeFaceSize() {
-		return mSharedPreferences.getString(showDetectionMaxRelativeFaceSizeKey, showDetectionMaxRelativeFaceSizeDefault);
+	private String detectionMaxRelativeFaceSizeKey;
+	private String detectionMaxRelativeFaceSizeDefault;
+	public double detectionMaxRelativeFaceSize() {
+		return Double.parseDouble(mSharedPreferences.getString(detectionMaxRelativeFaceSizeKey, detectionMaxRelativeFaceSizeDefault));
 	}
 	
-	private String showManagementNumberOfColumnsLandscapeKey;
-	private String showManagementNumberOfColumnsLandscapeDefault;
-	private String showManagementNumberOfColumnsLandscape () {
-		return mSharedPreferences.getString(showManagementNumberOfColumnsLandscapeKey, showManagementNumberOfColumnsLandscapeDefault);
+	private String managementNumberOfColumnsLandscapeKey;
+	private String managementNumberOfColumnsLandscapeDefault;
+	public int managementNumberOfColumnsLandscape () {
+		return Integer.parseInt(mSharedPreferences.getString(managementNumberOfColumnsLandscapeKey, managementNumberOfColumnsLandscapeDefault));
 	}
 	
-	private String showManagementNumberOfColumnsPortraitKey;
-	private String showManagementNumberOfColumnsPortraitDefault;
-	private String showManagementNumberOfColumnsPortrait () {
-		return mSharedPreferences.getString(showManagementNumberOfColumnsPortraitKey, showManagementNumberOfColumnsPortraitDefault);
+	private String managementNumberOfColumnsPortraitKey;
+	private String managementNumberOfColumnsPortraitDefault;
+	public int managementNumberOfColumnsPortrait () {
+		return Integer.parseInt(mSharedPreferences.getString(managementNumberOfColumnsPortraitKey, managementNumberOfColumnsPortraitDefault));
+	}
+	
+	private String detectionDetectorTypeKey;
+	private String detectionDetectorTypeDefault;
+	public String detectionDetectorType() {
+		return mSharedPreferences.getString(detectionDetectorTypeKey, detectionDetectorTypeDefault);
 	}
 }
